9fb2c0aab6c904ccbd3c79d835a00ffc0ccd15d7
TeraOwl <bulesop@gmail.com>
2022-01-02 07:23:05 +0100

    Add: prevent timeout getting data

--- src/helpers/getData.js
+++ src/helpers/getData.js
@@ -2,32 +2,34 @@ process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
 import axios from "axios-https-proxy-fix";
 import options from "./options.js";
 import UserAgent from "user-agents";
+import https from "https";
 
 let getData = async (url, responseType) => {
-    
-    const optionsToUse = (process.env.NODE_ENV === "production") ? options : {};
+    const optionsToUse = process.env.NODE_ENV === "production" ? options : {};
 
     const userAgent = new UserAgent({ deviceCategory: "mobile" });
     let UA = userAgent.random().data.userAgent;
     optionsToUse.headers = { "User-Agent": UA };
+    optionsToUse.timeout = 60000;
+    optionsToUse.httpsAgent = new https.Agent({ keepAlive: true });
+
     responseType && (optionsToUse.responseType = responseType);
-    
+
     let body = "";
     let error = false;
     let errorCount = 0;
-    if (url.includes("https://www.googleapis.com")){
-        console.log(url);
-    }
+
     do {
         try {
-            const {data} =  await axios.get(url, optionsToUse);
+            const { data } = await axios.get(url, optionsToUse);
             body = data;
             error = false;
         } catch (err) {
             console.log(err.message);
-            error = ( err?.response?.status !== 404 && err?.response?.status !==  503 && errorCount < 10);
+            error =
+                err?.response?.status !== 404 && err?.response?.status !== 503 && errorCount < 10;
             errorCount++;
-        } 
+        }
     } while (error);
     
     return body;
